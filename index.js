d3824ce18b27cfeded0d9f92ae67f8ff504ca37c
Ronan <ronan@ronanfuruta.com>
2021-02-12 22:36:00 -0800

    initial script

---
+++ index.js
@@ -0,0 +1,111 @@
+'use strict';
+const Hapi = require('@hapi/hapi');
+const Path = require('path');
+const fetch = require('node-fetch');
+const keys = require('./private/keys.json')
+var uaParser = require('ua-parser-js');
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://app:${keys.mongo.pass}@cluster0.zejsy.mongodb.net/analyticsDB?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const DBviews = client.db("analyticsDB").collection("views");
+  // perform actions on the collection object
+  const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost',
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    });
+    await server.register(require('@hapi/inert'));
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function(request, h) {
+
+            return h.file('testpage.html');
+        }
+    });
+    server.route({
+        method: 'GET',
+        path: '/test',
+        handler: function(request, h) {
+
+            return h.file('testpage.html');
+        }
+    });
+    server.route({
+        method: 'GET',
+        path: '/script',
+        handler: function(request, h) {
+
+            return h.file('script.js');
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/api/view',
+        handler: (request, h) => {
+            //console.log("info")
+           // console.log(request.info)
+            //console.log("headers")
+           // console.log(request.headers)
+            const xFF = request.headers['x-forwarded-for']
+            const ip = xFF ? xFF.split(',')[0] : request.info.remoteAddress
+            const clientData = request.payload.split(',')
+            let uaData = uaParser(request.headers["user-agent"])
+            fetch('http://ipwhois.app/json/' + ip)
+            .then(res => res.json())
+            .then(json => {
+              console.log(json["completed_requests"])
+              const data = {
+                propertyID: "propertyID",
+                pageurl: clientData[0],
+                time: clientData[1],
+                refferer: clientData[2],
+                userAgent: request.headers["user-agent"],
+                platform: clientData[4],
+                language: clientData[3],
+                screen: {
+                    width: clientData[5],
+                    height: clientData[6],
+                },
+                location: {
+                    country: json.country,
+                    flag: json["country_flag"],
+                    region: json.region,
+                    city: json.city
+                },
+                browser: {
+                    name: uaData.browser.name,
+                    version: uaData.browser.version,
+                    os: uaData.os.name,
+                }
+            }
+            console.log(data)
+            DBviews.insertOne(data)
+            });
+          
+
+            return true
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
+  //client.close();
+});
---
+++ public/script.js
@@ -0,0 +1,1 @@
+let d=[window.location.href,new Date().toISOString(),document.referrer,navigator["language"],navigator["platform"],window.screen.width,window.screen.height],x=new XMLHttpRequest();x.open('POST',"/api/view");x.setRequestHeader('Content-Type','text/plain');x.send(d);
