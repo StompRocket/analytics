0ab5c4ede8e951148724cb84b09d8016a34fb23f
Ronan <ronan@ronanfuruta.com>
2021-02-13 08:19:14 -0800

    comment oout

--- src/code.js
+++ src/code.js
@@ -1,9 +1,8 @@
 let s = function () {
 let d = [window.location.href, new Date().toISOString(), document.referrer, navigator["language"], navigator["platform"], window.screen.width, window.screen.height], x = new XMLHttpRequest();
-
-/*x.open('POST', "https://a.stomprocket.io/api/view");
+x.open('POST', "https://a.stomprocket.io/api/view");
 x.setRequestHeader('Content-Type', 'text/plain');
-x.send(d);*/
+x.send(d);
 console.log("send", d)
 }
 s()
