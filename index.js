c9b0d9dc4c792b8509d91108497626a00d6d4ffc
Ronan <rsf.sho@gmail.com>
2021-02-18 11:32:22 -0800

    now get data

--- index.js
+++ index.js
@@ -9,192 +9,271 @@ const uri = `mongodb+srv://app:${keys.mongo.pass}@cluster0.zejsy.mongodb.net/ana
 var admin = require("firebase-admin");
 var uuid = require('uuid');
 var serviceAccount = require("./private/fb.json");
-let mongoDB = new MongoClient({ useUnifiedTopology: true })
+let mongoDB = new MongoClient({
+    useUnifiedTopology: true
+})
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
-MongoClient.connect(uri,  function (err, client) {
+MongoClient.connect(uri, function (err, client) {
 
-  console.log(err)
-  const propertiesDB = client.db("analyticsDB").collection("properties");
+    console.log(err)
+    const propertiesDB = client.db("analyticsDB").collection("properties");
 
-//verifyToken("test")
-  // perform actions on the collection object
-  function getURLComponents(url) {
-    var pattern=/(.+:\/\/)?([^\/]+)(\/.*)*/i;
-    var arr=pattern.exec(url);
-    return {
-      url: arr[0],
-      protocal: arr[1],
-      hostname: arr[2],
-      page: arr[3]
+    //verifyToken("test")
+    // perform actions on the collection object
+    function getURLComponents(url) {
+        var pattern = /(.+:\/\/)?([^\/]+)(\/.*)*/i;
+        var arr = pattern.exec(url);
+        return {
+            url: arr[0],
+            protocal: arr[1],
+            hostname: arr[2],
+            page: arr[3]
 
+        }
     }
-  }
-  const init = async () => {
-    
+    const init = async () => {
+
         async function logView(view) {
 
-          const DBviews = client.db("analyticsDB").collection("views");
-          await DBviews.insertOne(view);
+            const DBviews = client.db("analyticsDB").collection("views");
+            await DBviews.insertOne(view);
 
         }
         async function verifyToken(token) {
-          try {
-            let decodedToken = await admin.auth().verifyIdToken(token);
-            const uid = decodedToken.uid;
-            console.log(uid, decodedToken);
-            return uid;
-          } catch {
-            console.log("token wrong");
-            return "1TSmFv3qGAgZySDjZO7flmVgOZq1";
-            //return false
-          }
+            try {
+                let decodedToken = await admin.auth().verifyIdToken(token);
+                const uid = decodedToken.uid;
+                console.log(uid, decodedToken);
+                return uid;
+            } catch {
+                console.log("token wrong");
+                return "1TSmFv3qGAgZySDjZO7flmVgOZq1";
+                //return false
+            }
 
 
         }
         const server = Hapi.server({
-          port: 3056,
-          host: 'localhost',
-          routes: {
-            "cors": true,
-            files: {
-              relativeTo: Path.join(__dirname)
+            port: 3056,
+            host: 'localhost',
+            routes: {
+                "cors": true,
+                files: {
+                    relativeTo: Path.join(__dirname)
+                }
             }
-          }
         });
         await server.register(require('@hapi/inert'));
         server.route({
-          method: 'GET',
-          path: '/',
-          handler: function (request, h) {
+            method: 'GET',
+            path: '/',
+            handler: function (request, h) {
 
-            return h.file('public/testpage.html');
-          }
+                return h.file('public/testpage.html');
+            }
         }); // /
         server.route({
-          method: 'GET',
-          path: '/test',
-          handler: function (request, h) {
+            method: 'GET',
+            path: '/test',
+            handler: function (request, h) {
 
-            return h.file('public/testpage.html');
-          }
+                return h.file('public/testpage.html');
+            }
         }); // /test
         server.route({
-          method: 'GET',
-          path: '/script',
-          handler: function (request, h) {
+            method: 'GET',
+            path: '/script',
+            handler: function (request, h) {
 
-            return h.file('src/code.js');
-          }
+                return h.file('src/code.js');
+            }
         }); // /script
         server.route({
-          method: 'POST',
-          path: '/api/v1/view',
-          handler: async (request, h) => {
-            const xFF = request.headers['x-forwarded-for'];
-            const ip = xFF ? xFF.split(',')[0] : request.info.remoteAddress;
-            const clientData = request.payload.split(',');
-            let uaData = uaParser(request.headers["user-agent"]);
-            let urlComponents = getURLComponents(clientData[0])
+            method: 'POST',
+            path: '/api/v1/view',
+            handler: async (request, h) => {
+                const xFF = request.headers['x-forwarded-for'];
+                const ip = xFF ? xFF.split(',')[0] : request.info.remoteAddress;
+                const clientData = request.payload.split(',');
+                let uaData = uaParser(request.headers["user-agent"]);
+                let urlComponents = getURLComponents(clientData[0])
                 console.log(urlComponents)
-                let existing = await propertiesDB.find({ domain: urlComponents.hostname }).toArray();
+                let existing = await propertiesDB.find({
+                    domain: urlComponents.hostname
+                }).toArray();
                 if (existing.length > 0) {
-                  
-                
-               console.log(existing)
-            fetch('http://ipwhois.app/json/' + ip)
-              .then(res => res.json())
-              .then(async json => {
-                console.log(json["completed_requests"], request.info.host);
-                const data = {
-                  propertyID: existing[0]["_id"],
-                  pageurl: clientData[0],
-                  time: clientData[1],
-                  refferer: clientData[2],
-                  userAgent: request.headers["user-agent"],
-                  platform: clientData[4],
-                  language: clientData[3],
-                  screen: {
-                    width: clientData[5],
-                    height: clientData[6],
-                  },
-                  location: {
-                    country: json.country,
-                    flag: json["country_flag"],
-                    region: json.region,
-                    city: json.city
-                  },
-                  browser: {
-                    name: uaData.browser.name,
-                    version: uaData.browser.version,
-                    os: uaData.os.name,
-                  }
-                };
-                console.log(data);
-                logView(data);
-              });
 
-            } else {
-              console.log("not registered domain")
+
+                    console.log(existing)
+                    fetch('http://ipwhois.app/json/' + ip)
+                        .then(res => res.json())
+                        .then(async json => {
+                            console.log(json["completed_requests"], request.info.host);
+                            const data = {
+                                propertyID: existing[0]["_id"],
+                                pageurl: clientData[0],
+                                time: clientData[1],
+                                refferer: clientData[2],
+                                userAgent: request.headers["user-agent"],
+                                platform: clientData[4],
+                                language: clientData[3],
+                                screen: {
+                                    width: clientData[5],
+                                    height: clientData[6],
+                                },
+                                location: {
+                                    country: json.country,
+                                    flag: json["country_flag"],
+                                    region: json.region,
+                                    city: json.city
+                                },
+                                browser: {
+                                    name: uaData.browser.name,
+                                    version: uaData.browser.version,
+                                    os: uaData.os.name,
+                                }
+                            };
+                            console.log(data);
+                            logView(data);
+                        });
+
+                } else {
+                    console.log("not registered domain")
+                }
+                return true;
             }
-            return true;
-          }
         }); // POST /api/v1/view
         server.route({
-          method: 'POST',
-          path: '/api/v1/property',
-          handler: async function  (request, h) {
-            let body = request.payload;
-            let uid = await verifyToken(body.auth);
-            console.log(body, uid);
-           
-            if (uid) {
+            method: 'POST',
+            path: '/api/v1/property',
+            handler: async function (request, h) {
+                let body = request.payload;
+                let uid = await verifyToken(body.auth);
+                console.log(body, uid);
 
-              
-              let existing = await propertiesDB.find({ domain: body.domain }).toArray();
-              console.log(existing)
-              if (existing.length > 0) {
-                return h.response({ success: false, error: "domain in use" }).code(401);
-
-              } else {
-
-                const propertiesDB = client.db("analyticsDB").collection("properties");
-                let id = uuid.v4();
-                //console.log(body, body.domain)
-                propertiesDB.insertOne({
-                  _id: id,
-                  domain: body.domain,
-                  access: [uid],
-                  created: new Date().toISOString()
-                });
-                return h.response({ success: true, id: id }).code(200);
+                if (uid) {
 
 
+                    let existing = await propertiesDB.find({
+                        domain: body.domain
+                    }).toArray();
+                    console.log(existing)
+                    if (existing.length > 0) {
+                        return h.response({
+                            success: false,
+                            error: "domain in use"
+                        }).code(401);
 
-              }
+                    } else {
+
+                        const propertiesDB = client.db("analyticsDB").collection("properties");
+                        let id = uuid.v4();
+                        //console.log(body, body.domain)
+                        propertiesDB.insertOne({
+                            _id: id,
+                            domain: body.domain,
+                            access: [uid],
+                            created: new Date().toISOString()
+                        });
+                        return h.response({
+                            success: true,
+                            id: id
+                        }).code(200);
 
 
 
-            } else {
-              return h.response({ success: false, error: "not authorized" }).code(401);
+                    }
+
+
+
+                } else {
+                    return h.response({
+                        success: false,
+                        error: "not authorized"
+                    }).code(401);
+                }
             }
-          }
-        }); // POST /api/v1/property
-        
+        }); // GET /api/v1/property
+        server.route({
+            method: 'POST',
+            path: '/api/v1/data/{propertyID}',
+            handler: async function (request, h) {
+                let body = request.payload;
+                let uid = await verifyToken(body.auth);
+                console.log(request.params.propertyID, uid);
+                if (uid) {
+                    let existing = await propertiesDB.find({
+                        "_id": request.params.propertyID
+                    }).toArray();
+                    console.log(existing)
+                    if (existing.length > 0) {
+                        let property = existing[0]
+                        if (property.access.indexOf(uid) > -1) {
+                            const dataDB = client.db("analyticsDB").collection("views");
+                            let data = [];
+                           // console.log(new Date(2021, 1, 1).toISOString(), new Date().toISOString())
+                            try {
+                                data = await dataDB.find({
+                                    "propertyID": request.params.propertyID,
+                                    time: {
+                                        $gte: body.from || new Date(2021, 1, 1).toISOString(),
+                                        $lt: body.to || new Date().toISOString()
+                                    }
+                                }).toArray()
+                            } catch {
+                                data = []
+                            }
+                           
+
+
+                            return h.response({
+                                success: true,
+                                id: request.params.propertyID,
+                                data: data
+                            }).code(200);
+                        } else {
+                            return h.response({
+                                success: false,
+                                error: "not authorized"
+                            }).code(401);
+                        }
+
+
+
+                    } else {
+                        return h.response({
+                            success: false,
+                            error: "property doesn't exist"
+                        }).code(401);
+
+
+                    }
+
+
+
+                } else {
+                    return h.response({
+                        success: false,
+                        error: "not authorized"
+                    }).code(401);
+                }
+            }
+        }); // POST /api/v1/data/{property id}
 
 
         await server.start();
         console.log('Server running on %s', server.info.uri);
-      
-};
 
-process.on('unhandledRejection', (err) => {
+    };
 
-    console.log(err);
-    process.exit(1);
-});
+    process.on('unhandledRejection', (err) => {
 
-init();
+        console.log(err);
+        process.exit(1);
+    });
+
+    init();
 })
